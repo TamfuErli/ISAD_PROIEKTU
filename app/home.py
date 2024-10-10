bc8ecba226869b9bfef55645bfbc0d3bc49537cc
Erlantz Tamfumu <tamfuerli@gmail.com>
2024-10-10 11:17:57 +0200

    Deleted innecesary things

--- app/home.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-from flask import request
-
-
-def home_index():
-    return render_template('home.html')
-
-
---
+++ main.py
@@ -0,0 +1,3 @@
+from app import pagina_kontrola
+
+pagina_kontrola.app.run(debug=True)
