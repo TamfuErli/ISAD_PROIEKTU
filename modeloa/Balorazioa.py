ae2bf52e15885e3b30de409607ec8251d67fba6b
aimarbasterretxea <162178939+aimarbasterretxea@users.noreply.github.com>
2025-01-18 16:48:15 +0100

    balorazioa.py aldaketak
    balorazio zerrenda igual no se necesita,

--- modeloa/Balorazioa.py
+++ modeloa/Balorazioa.py
@@ -1,11 +1,35 @@
-import sqlite3 
-from Connection import Connection
+import sqlite3
 
-db = sqlite3.Connection()
+def balorazioa_gorde(pelicula_id, usuario_id, puntuacion, comentario):
+    """
+    Guarda la valoración de un usuario en la base de datos.
+    """
+    connection = sqlite3.connect('datuBasea.db')
+    cursor = connection.cursor()
 
-class Balorazioa:
-    def __init__(self, balioa, textua, filmId):
-        self.balioa = balioa
-        self.textua = textua
-        self.filmId = filmId
-        
+    cursor.execute("""
+        INSERT INTO valoraciones (pelicula_id, usuario_id, puntuacion, comentario)
+        VALUES (?, ?, ?, ?)
+    """, (pelicula_id, usuario_id, puntuacion, comentario))
+
+    connection.commit()
+    connection.close()
+
+def get_balorazioa(pelicula_id):
+    """
+    Obtiene todas las valoraciones de una película.
+    """
+    connection = sqlite3.connect('datuBasea.db')
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT usuario_id, puntuacion, comentario, fecha
+        FROM valoraciones
+        WHERE pelicula_id = ?
+        ORDER BY fecha DESC
+    """, (pelicula_id,))
+
+    valoraciones = cursor.fetchall()
+    connection.close()
+
+    return valoraciones
